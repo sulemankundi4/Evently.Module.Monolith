a131e3ed96991a487fc7157119366150ee10f49a
sulemankundi4 <msulemankdi@gmail.com>
2026-01-26 15:39:15 +0500

    Add project files.

---
+++ Evently.Modules.Events.Api/Database/EventsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Evently.Modules.Events.Api.Database;
+
+public sealed class EventsDbContext(DbContextOptions<EventsDbContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema(Schemas.Events);
+    }
+
+    internal DbSet<Events.Event> Events { get; set; }
+}
---
+++ Evently.Modules.Events.Api/Events/EventResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Evently.Modules.Events.Api.Events;
+
+internal sealed record EventResponse(
+    string Title,
+    string Description,
+    string Location,
+    DateTime StartsAtUtc,
+    DateTime? EndsAtUtc
+);
